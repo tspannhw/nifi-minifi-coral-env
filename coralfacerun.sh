bfdd4aeb9d600ecd8a97e1cfd141145128f9cd60
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2019-06-25 12:52:14 -0400

    Create coralfacerun.sh

---
+++ coralfacerun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H%M")
+
+fswebcam -q -r 1280x720 --no-banner /opt/demo/images/$DATE.jpg
+python3 -W ignore /opt/demo/face.py --image /opt/demo/images/$DATE.jpg 2>/dev/null
